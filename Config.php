5453f3491255abe5e5ec314901c49a6313607922
Samuel H <sam.hilarus@gmail.com>
2017-03-05 17:59:19 +0100

    Ajout path fpdf + mise en forme exportcontroller

--- Config.php
+++ Config.php
@@ -8,7 +8,7 @@
 		const DB_PASS = "";
 		const DB_NAME = "song_repository";
 
-		static public $class_folders = array(__DIR__, "Modele", "Vue", "Controleur", "Utils", "Utils/kint");
+		static public $class_folders = array(__DIR__, "Utils/fpdf","Modele", "Vue", "Controleur", "Utils", "Utils/kint");
 		const HOME_CONTROLLER = "SongController";
 		const HOME_METHOD = "homeAction";
 		const BASE_URL = "localhost/SongRepository/";
--- Controleur/ExportController.php
+++ Controleur/ExportController.php
@@ -1,9 +1,20 @@
 <?php
-require('fpdf.php');
+    class ExportController extends Controller{
+        
+        public function __construct(){
+        }
+                
+        public function toPdfAction() {
+            $pdf = new FPDF();
+            $pdf->AddPage();
+            $pdf->SetFont('Arial','B',16);
+            $pdf->Cell(40,10,'Hello World !');
+            $pdf->Output();
 
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World !');
-$pdf->Output();
+            //$params = array( "songs"=>$lastSongs,  "mostViewedSongs"=>$mostViewedSongs,  "intro"=>$intro);
+            //new View("accueil.php", Config::APP_NAME, $params);
+
+        }
+
+}
 ?>
