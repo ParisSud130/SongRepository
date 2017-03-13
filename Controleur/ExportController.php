beb7d298dc498404164edb52cc4f467c215d09b4
Samuel H <sam.hilarus@gmail.com>
2017-03-13 13:26:27 +0100

    MPD - Export chant pdf - Recueil H&L
    MPD : Ajout d'un auto-incrément sur la clé primaire de la table trame
    Export chant pdf : Mise en forme (gras italique taille) et ajout du nom
    du recueil et du numéro du chant dans le recueil
    Recueil H&L : Ajout du recueil Hymnes et Louanges dans la base

--- Controleur/ExportController.php
+++ Controleur/ExportController.php
@@ -10,9 +10,6 @@
         }
                 
         public function trameToPdfAction($id) {
-            
-
-
         }
 
         public function chantToPdfAction() {
@@ -20,31 +17,47 @@
                 require (__DIR__."/../Utils/fpdf/fpdf.php");
             }
             if (isset($_GET["id"])){
-                $id = $_GET["id"];
-                $texte = $this->chantToTexte($id);
-
                 $pdf = new FPDF();
-                $pdf->AddFont('Trebuchet','','trebuc.php');
                 $pdf->AddPage();
-                $pdf->SetFont('Trebuchet','',12);
-                $pdf->MultiCell(0,10,$texte);
+
+                $this->writeSong($pdf, $_GET["id"]);
+
                 $pdf->Output();
             }
         }
 
-        private function chantToTexte($id) {
-            $song = $this->sm->getSong($id);
-            $text = utf8_decode($song->getTitre()) . "\n\n";
-            
-            foreach ($song->getStrophes() as $strophe){
-                $text .= $strophe->getType(). " ".$strophe->getIdentifiant() . "\n";
-                $strophe->setTexte(str_replace("œ","oe",$strophe->getTexte()));
-                $strophe->setTexte(str_replace("œ","oe",preg_replace( "/[\r\n]+/", "\n", $strophe->getTexte() )));
-                $text .= utf8_decode($strophe->getTexte()) . "\n\n"; 
-            }  
+        private function writeSong($_pdf, $idChant) {
+            $song = $this->sm->getSong($idChant);
+            $this->writeTitre($_pdf, $song);
 
-            return $text;          
+            foreach ($song->getStrophes() as $strophe){
+                $this->writeStrophe($_pdf, $strophe);
+            }  
         }
 
-}
+        private function writeTitre($_pdf, $_song) {
+            $_pdf->SetFont('Helvetica', 'b', 20);
+            $_pdf->Write(10, utf8_decode($_song->getTitre()));
+            $_pdf->Ln();
+            $_pdf->SetFont('Helvetica', 'bi', 14);
+            $_pdf->Write(7, '(' . utf8_decode($_song->getRecueil()->getNomRecueil()) . ' # ' . $_song->getNumChant() . ')');
+            $_pdf->Ln();
+            $_pdf->Ln();
+        }
+
+        private function writeStrophe($_pdf, $_strophe) {
+            
+            $_pdf->SetFont('Helvetica', 'bi', 12);
+            $_pdf->Write(7, $_strophe->getType(). " ".$_strophe->getIdentifiant());
+            $_pdf->Ln();
+
+            $_pdf->SetFont('Helvetica', '', 12);
+            $_strophe->setTexte(str_replace("œ","oe",$_strophe->getTexte()));
+            $_strophe->setTexte(str_replace("œ","oe",preg_replace( "/[\r\n]+/", "\n", $_strophe->getTexte() )));
+            $_pdf->Write(7, utf8_decode($_strophe->getTexte()));
+            $_pdf->Ln();
+            $_pdf->Ln();
+        }
+    }
+
 ?>
