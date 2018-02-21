42db4cf22d3a7ea5aef751abd282cec649db9ad3
Samuel H <sam.hilarus@gmail.com>
2018-02-21 22:49:02 +0100

    Ajout date de création
    Ajout date création

--- Modele/Trame.php
+++ Modele/Trame.php
@@ -3,6 +3,7 @@
 class Trame{
 	
 	private $_idTrame;				//Identifiant de la trame
+	private $_dateCreation;			//Date de création de la trame
 	private $_dateModification;		//Date de dernière modification de la trame
 	private $_dateExecution;		//Date d'exécution de la trame
 	private $_proprietaire;			//propriétaire/créateur de la trame
@@ -18,6 +19,9 @@ class Trame{
 					
 					case "_idTrame":
 						$this->_idTrame = $value;
+						break;
+					case "_dateCreation":
+						$this->_dateCreation = $value;
 						break;
 					case "_dateModification":
 						$this->_dateModification = $value;
@@ -40,33 +44,31 @@ class Trame{
 
 	//getters et setters
 
-		
 	public function getIdTrame(){ return $this->_idChant; }
 	public function setIdTrame($idChant){ $this->_idChant = $idChant; }
-
+		
+	public function getDateCreation(){ return $this->_dateCreation; }
+	public function setDateCreation($dateCreation){ $this->_dateCreation = $dateCreation; }
 		
 	public function getDateModification(){ return $this->_dateModification; }
 	public function setDateModification($dateModification){ $this->_dateModification = $dateModification; }
-
 		
 	public function getIdDateExecution(){ return $this->_idDateExecution; }
 	public function setIdDateExecution($idDateExecution){ $this->_idDateExecution = $idDateExecution; }
-
 		
 	public function getProprietaire(){ return $this->_proprietaire; }
 	public function setProprietaire($proprietaire){ $this->_proprietaire = $proprietaire; }
-
 		
 	public function getCommentaire(){ return $this->_commentaire; }
 	public function setCommentaire($commentaire){ $this->_commentaire = $commentaire; }
 
-		
 	public function getChants(){ return $this->_chants; }
 	public function setChants(array $chants){ 
 		if(is_array($chants) && !empty($chants)){
 			$this->chants = $chants; 
 		}
 	}
+
 	public function getChant($numChant){ return $this->_chants[$numChant]; }
 }
 ?>
